a4fca1d4901dc086c8cd2ae33042fb32e34f0b0d
unknown <hansingjunitra@outlook.com>
2021-03-18 00:31:26 +0800

    added routing to nodes from client

--- client/client.go
+++ client/client.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	// "bufio"
 	"bufio"
+	"crypto/md5"
+	"encoding/binary"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
 )
@@ -33,21 +39,55 @@ func main() {
 			input := string(raw_input[:len(raw_input)-1])
 			input_array := strings.Split(input, " ")
 
-			if len(input_array) != 3 {
+			if len(input_array) < 2 {
 				fmt.Println("Invalid Input!")
 			}
 
-			clientQuery := ClientQuery{
+			cq := ClientQuery{
 				command: input_array[0],
 				key:     input_array[1],
-				value:   input_array[2],
+				value:   strings.Join(input_array[2:], " "),
 			}
 
-			fmt.Printf("Send request to server ----- %v Key: %v with Value: %v\n", clientQuery.command, clientQuery.key, clientQuery.value)
+			/*
+				Dynamo treats both the key and the object supplied by the caller  as an opaque array of bytes. It applies a MD5 hash on the key to
+				generate a 128-bit identifier, which is used to determine the storage nodes that are responsible for serving the key
+			*/
+
+			// Calculate hash value
+			hash := md5.Sum([]byte(cq.key))
+			i := binary.LittleEndian.Uint64(hash[:])
+			fmt.Printf("Looking for Nodes that handles: %v\n", i)
+
+			// port_number := "5000"
+
+			// Calculate which node handles
+
+			fmt.Printf("Send request to server ----- %v Key: %v with Value: %v\n", cq.command, cq.key, cq.value)
 
 			// send http request here and wait for reply
 
-			// resp, err := http.PostForm("http://localhost:5000/", url.Values{"key": {clientQuery.key}, "value": {clientQuery.value}})
+			if strings.Contains(cq.command, "store") {
+				resp, err := http.PostForm("http://localhost:5000/set", url.Values{"key": {cq.key}, "value": {cq.value}})
+				if err != nil {
+					// handle error
+					fmt.Println(err)
+				}
+				defer resp.Body.Close()
+				body, err := ioutil.ReadAll(resp.Body)
+				fmt.Println(string(body))
+			}
+
+			if strings.Contains(cq.command, "fetch") {
+				resp, err := http.PostForm("http://localhost:5000/get", url.Values{"key": {cq.key}})
+				if err != nil {
+					// handle error
+					fmt.Println(err)
+				}
+				defer resp.Body.Close()
+				body, err := ioutil.ReadAll(resp.Body)
+				fmt.Println(string(body))
+			}
 
 		}
 	}
