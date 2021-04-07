a26358f1c6d89d0598f7d7a5030ae8b4a32d9a42
AnAsianGangster <yangaobo1996@gmail.com>
2021-04-07 11:03:32 +0800

    [fix] the port not in env issue

--- main.go
+++ main.go
@@ -1,3 +1,14 @@
+/*
+ * Author: Yang Aobo
+ * Telegram: @AnAsianGangster
+ * Created At: March 17, 2021
+ * Updated At: April 7, 2021
+ * Last Modified By: Yang Aobo
+ */
+
+/**
+ * This is the main package, the entry point
+ */
 package main
 
 import (
@@ -14,6 +25,15 @@ func main() {
 	// mount router to routes
 	routers.MountDatabaseIORouter(router)
 	routers.ServerOperation(router)
+	routers.NodeHealth(router)
 
-	router.Run(":" + os.Getenv("SERVER_PORT"))
+	// check if running server on go-http-redis independently or under docker
+	var PORT string
+	if os.Getenv("SERVER_PORT") == "" {
+		PORT = "5000"
+	} else {
+		PORT = os.Getenv("SERVER_PORT")
+	}
+
+	router.Run(":" + PORT)
 }
