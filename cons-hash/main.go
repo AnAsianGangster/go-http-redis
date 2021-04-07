70a382da29cd5da1dc51583be624f0370271bdd9
unknown <hansingjunitra@outlook.com>
2021-04-07 22:36:06 +0800

    fix hash function result

--- cons-hash/main.go
+++ cons-hash/main.go
@@ -10,7 +10,7 @@ func getHashValue(key string) uint64 {
 	// Calculate hash value
 	hash := md5.Sum([]byte(key))
 	fmt.Println(hash)
-	value := binary.LittleEndian.Uint64(hash[:8])
+	value := binary.LittleEndian.Uint64(hash[0:8])
 
 	return value
 }
@@ -19,17 +19,18 @@ func main() {
 
 	// Listen at some port
 
-	number_of_nodes := 3
+	number_of_nodes := 10
 	node_value_mapping := make([]uint64, number_of_nodes)
-	MAX_VAL := 9223372036854775807
+	MAX_VAL := ^uint64(0)
 
 	for i := 0; i < number_of_nodes; i++ {
 		// 64* bit / number of nodes * (node id)
-		node_value_mapping[i] = uint64(MAX_VAL / number_of_nodes * i)
+		node_value_mapping[i] = uint64(MAX_VAL / uint64(number_of_nodes) * uint64(i))
 	}
 
 	// When the client ask the node
-	hash := getHashValue("keys14d")
+	hash := getHashValue("aasdfsasdaasdf132sdf313")
+	fmt.Println(hash)
 	var idx int
 	for i := number_of_nodes - 1; i >= 0; i-- {
 		// 64* bit / number of nodes * (node id)
@@ -40,9 +41,5 @@ func main() {
 	}
 
 	fmt.Println("this is the main.go for go-consistent-hashing", idx)
-
-	// println(getHashValue(key))
-
-	// For demo purpose
 
 }
